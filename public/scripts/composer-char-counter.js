f0faa9d5228f72eb5b748a6db5899cc7704e1b64
Alejandra Aguilar <aleaguilar01@gmail.com>
2024-04-13 00:48:39 -0600

    Implement AI suggestions

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,9 +1,9 @@
 const MAX_CHARS = 140;
 
 $(() => {
-  $(".tweet-text").on("keyup" , function() {
-    let remainingChars = MAX_CHARS - this.value.length;
-    const counterElement = $(this).next().children(".counter")[0];
+  $(".tweet-text").on("input" , function() {
+    let remainingChars = MAX_CHARS - $(this).val().length;
+    const counterElement = $(this).closest('form').find('.counter')[0];
     counterElement.innerHTML = remainingChars;
     if (remainingChars < 0) {
       $(counterElement).addClass("counter-red");
