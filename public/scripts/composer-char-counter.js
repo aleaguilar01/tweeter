c99ae70adf3c598db4200e65a73445159253dfd4
Alejandra Aguilar <aleaguilar01@gmail.com>
2024-04-13 00:41:43 -0600

    Implement AI suggestions.

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,14 +1,14 @@
 const MAX_CHARS = 140;
 
 $(() => {
-  $(".tweet-text").on("input" , function() {
+  $(".tweet-text").on("keyup" , function() {
     let remainingChars = MAX_CHARS - this.value.length;
     const counterElement = $(this).next().children(".counter")[0];
     counterElement.innerHTML = remainingChars;
     if (remainingChars < 0) {
-      $(counterElement).addClass("error");
+      $(counterElement).addClass("counter-red");
     } else {
-      $(counterElement).removeClass("error");
+      $(counterElement).removeClass("counter-red");
     }
   });
 });
